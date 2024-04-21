45993882578037cd76d5297969575b4970e662b3
Tharushika Dilmini <dilz2001.1221@gmail.com>
2024-04-21 11:41:21 +0530

    trying for datatypes

---
+++ First_Week/DATATYPES.py
@@ -0,0 +1,25 @@
+
+x = "Python{}"
+y = "Elephant"
+z = """\nHellow World"""
+
+# print(x)
+# print(type(x))
+# print(type(x), x)
+#checked the type
+print(x,y,z)
+print(x[0])
+print(x[4]) 
+# this can also be done by -2
+
+a = 0
+print(type(a))
+print(x.format(a))
+print(x)
+print(x + y+ str(a))
+
+# another way to use formatt
+print(f"White {y}")
+
+b = 0.0
+print(type(b))
